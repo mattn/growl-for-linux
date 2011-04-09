aa877cda873da19380c636c5897af655d268ad32
mattn <mattn.jp@gmail.com>
2011-04-10 01:24:24 +0900

    multi-thread.

--- gol.c
+++ gol.c
@@ -44,6 +44,7 @@
 #include <openssl/des.h>
 
 #ifdef _WIN32
+typedef char sockopt_t;
 typedef int socklen_t;
 # ifndef snprintf
 #  define snprintf _snprintf
@@ -59,6 +60,7 @@ typedef int socklen_t;
 # endif
 #else
 # define closesocket(x) close(x)
+typedef int sockopt_t;
 #endif
 
 #define REQUEST_TIMEOUT            (5)
@@ -68,6 +70,8 @@ static gchar* password = "123456"; // should be configuable.
 static gboolean main_loop = TRUE;
 
 typedef struct {
+  int sock;
+
   gint pos;
   gint x, y;
   gchar* title;
@@ -253,14 +257,13 @@ open_url(const gchar* url) {
 #if defined(_WIN32)
   r = (int) ShellExecute(NULL, "open", url, NULL, NULL, SW_SHOW);
 #elif defined(MACOSX)
-  gchar* command = g_strdup_printf("open '%s'", url);
-  g_spawn_command_line_async(command, NULL);
-  g_free(command);
+  GError* error = NULL;
+  gchar *argv[] = {"open", url, NULL};
+ 	g_spawn_async(NULL, argv, NULL, G_SPAWN_SEARCH_PATH, NULL, NULL, NULL, &error);
 #else
-printf("%s\n", url);
-  gchar* command = g_strdup_printf("xdg-open '%s'", url);
-  g_spawn_command_line_async(command, NULL);
-  g_free(command);
+  GError* error = NULL;
+  gchar *argv[] = {"xdg-open", url, NULL};
+ 	g_spawn_async(NULL, argv, NULL, G_SPAWN_SEARCH_PATH, NULL, NULL, NULL, &error);
 #endif
   return r;
 }
@@ -276,9 +279,12 @@ static gboolean
 popup_animation_func(gpointer data) {
   POPUP_INFO* pi = (POPUP_INFO*) data;
 
+  gdk_threads_enter();
+
   if (pi->timeout-- < 0) {
     gtk_widget_destroy(pi->popup);
     popup_list = g_list_remove(popup_list, pi);
+    gdk_threads_leave();
     g_free(pi->title);
     g_free(pi->text);
     g_free(pi->icon);
@@ -297,6 +303,7 @@ popup_animation_func(gpointer data) {
   if (pi->timeout < 30) {
     gtk_window_set_opacity(GTK_WINDOW(pi->popup), (double) pi->timeout/30.0*0.8);
   }
+  gdk_threads_leave();
   return TRUE;
 }
 
@@ -306,9 +313,7 @@ popup_list_compare(gconstpointer a, gconstpointer b) {
 }
 
 static void
-popup_show(
-    const gchar* title, const gchar* text,
-    const gchar* icon, const gchar* url) {
+popup_show(POPUP_INFO* pi) {
   GdkColor color;
   gdk_color_parse ("white", &color);
   GtkWidget* fixed;
@@ -323,6 +328,7 @@ popup_show(
   gint monitor_num;
   GdkRectangle rect;
 
+  gdk_threads_enter();
   len = g_list_length(popup_list);
   for (pos = 0; pos < len; pos++) {
     POPUP_INFO* p = g_list_nth_data(popup_list, pos);
@@ -346,13 +352,8 @@ popup_show(
     }
   }
 
-  POPUP_INFO* pi = g_new0(POPUP_INFO, 1);
   pi->pos = pos;
   popup_list = g_list_insert_sorted(popup_list, pi, popup_list_compare);
-  pi->title = (gchar*) g_strdup(title);
-  pi->text = (gchar*) g_strdup(text);
-  pi->icon = (gchar*) g_strdup(icon);
-  pi->url = (gchar*) g_strdup(url);
   pi->x = x;
   pi->y = y + 200;
 
@@ -408,6 +409,7 @@ popup_show(
 #endif
   
   g_timeout_add(10, popup_animation_func, pi);
+  gdk_threads_leave();
 }
 
 static long
@@ -432,12 +434,182 @@ unhex(unsigned char c) {
 
 static void
 statusicon_popup(GtkStatusIcon* status_icon, guint button, guint32 activate_time, gpointer menu) {
+  gdk_threads_enter();
   gtk_menu_popup(GTK_MENU(menu), NULL, NULL, gtk_status_icon_position_menu, status_icon, button, activate_time);
+  gdk_threads_leave();
 }
 
 static void
 exit_clicked(GtkWidget* widget, GdkEvent* event, gpointer user_data) {
   main_loop = FALSE;
+}
+
+static gpointer
+recv_thread(gpointer data) {
+  POPUP_INFO* pi = (POPUP_INFO*) data;
+  sockopt_t sockopt;
+
+  sockopt = 1;
+  setsockopt(pi->sock, IPPROTO_TCP, TCP_NODELAY, &sockopt, sizeof(sockopt));
+  char* ptr;
+  int r = readall(pi->sock, &ptr);
+  char* top = ptr;
+  if (!strncmp(ptr, "GNTP/1.0 ", 9)) {
+    ptr += 9;
+    if (!strncmp(ptr, "REGISTER ", 9)) {
+      ptr += 9;
+    } else
+    if (!strncmp(ptr, "NOTIFY ", 7)) {
+      ptr += 7;
+      char* data = NULL;
+      if (!strncmp(ptr, "NONE ", 5)) {
+        ptr = strchr(ptr, '\r');
+        *ptr++ = 0;
+        *ptr++ = 0;
+        data = (char*) calloc(r-(ptr-top)-4+1, 1);
+        memcpy(data, ptr, r-(ptr-top)-4);
+      } else {
+        if (strncmp(ptr, "AES:", 4) &&
+            strncmp(ptr, "DES:", 4) &&
+            strncmp(ptr, "3DES:", 5)) {
+          g_free(pi);
+          return NULL;
+        }
+
+        char* crypt_algorythm = ptr;
+        ptr = strchr(ptr, ':');
+        *ptr++ = 0;
+        char* iv;
+        iv = ptr;
+        ptr = strchr(ptr, ' ');
+        *ptr++ = 0;
+
+        if (strncmp(ptr, "MD5:", 4) &&
+            strncmp(ptr, "SHA1:", 5) &&
+            strncmp(ptr, "SHA256:", 7)) {
+          g_free(pi);
+          return NULL;
+        }
+
+        char* hash_algorythm = ptr;
+        ptr = strchr(ptr, ':');
+        *ptr++ = 0;
+        char* key = ptr;
+        ptr = strchr(ptr, '.');
+        *ptr++ = 0;
+        char* salt = ptr;
+
+        ptr = strchr(ptr, '\r');
+        *ptr++ = 0;
+        *ptr++ = 0;
+
+        int n, keylen, saltlen, ivlen;
+
+        char hex[3];
+        hex[2] = 0;
+        saltlen = strlen(salt) / 2;
+        for (n = 0; n < saltlen; n++)
+          salt[n] = unhex(salt[n * 2]) * 16 + unhex(salt[n * 2 + 1]);
+        keylen = strlen(key) / 2;
+        for (n = 0; n < keylen; n++)
+          key[n] = unhex(key[n * 2]) * 16 + unhex(key[n * 2 + 1]);
+        ivlen = strlen(iv) / 2;
+        for (n = 0; n < ivlen; n++)
+          iv[n] = unhex(iv[n * 2]) * 16 + unhex(iv[n * 2 + 1]);
+
+        char digest[32] = {0};
+        memset(digest, 0, sizeof(digest));
+
+        if (!strcmp(hash_algorythm, "MD5")) {
+          MD5_CTX ctx;
+          MD5_Init(&ctx);
+          MD5_Update(&ctx, password, strlen(password));
+          MD5_Update(&ctx, salt, saltlen);
+          MD5_Final((unsigned char*) digest, &ctx);
+        }
+        if (!strcmp(hash_algorythm, "SHA1")) {
+          SHA_CTX ctx;
+          SHA1_Init(&ctx);
+          SHA1_Update(&ctx, password, strlen(password));
+          SHA1_Update(&ctx, salt, saltlen);
+          SHA1_Final((unsigned char*) digest, &ctx);
+        }
+        if (!strcmp(hash_algorythm, "SHA256")) {
+          SHA256_CTX ctx;
+          SHA256_Init(&ctx);
+          SHA256_Update(&ctx, password, strlen(password));
+          SHA256_Update(&ctx, salt, saltlen);
+          SHA256_Final((unsigned char*) digest, &ctx);
+        }
+
+        data = (char*) calloc(r, 1);
+        if (!strcmp(crypt_algorythm, "AES")) {
+          AES_KEY aeskey;
+          AES_set_decrypt_key((unsigned char*) digest, 24 * 8, &aeskey);
+          AES_cbc_encrypt((unsigned char*) ptr, (unsigned char*) data,
+              r-(ptr-top)-6, &aeskey, (unsigned char*) iv, AES_DECRYPT);
+        }
+        if (!strcmp(crypt_algorythm, "DES")) {
+          des_key_schedule schedule;
+          DES_set_key_unchecked((const_DES_cblock*) &digest, &schedule);
+          DES_ncbc_encrypt((unsigned char*) ptr, (unsigned char*) data,
+              r-(ptr-top)-6, &schedule, (const_DES_cblock*) &iv, DES_DECRYPT);
+        }
+        if (!strcmp(crypt_algorythm, "3DES")) {
+          char key1[8], key2[8], key3[8];
+          memcpy(key1, digest+ 0, 8);
+          memcpy(key2, digest+ 8, 8);
+          memcpy(key3, digest+16, 8);
+          des_key_schedule schedule1, schedule2, schedule3;
+          DES_set_key_unchecked((const_DES_cblock*) &key1, &schedule1);
+          DES_set_key_unchecked((const_DES_cblock*) &key2, &schedule2);
+          DES_set_key_unchecked((const_DES_cblock*) &key3, &schedule3);
+          des_ede3_cbc_encrypt((unsigned char*) ptr, (unsigned char*) data,
+              r-(ptr-top)-6, schedule1, schedule2, schedule3,
+              (const_DES_cblock*) &iv, DES_DECRYPT);
+        }
+      }
+
+      ptr = data;
+      while (*ptr) {
+        char* line = ptr;
+        ptr = strchr(ptr, '\r');
+        if (!ptr) break;
+        *ptr++ = 0;
+        *ptr++ = 0;
+        if (!strncmp(line, "Notification-Title:", 19)) {
+          line += 20;
+          while(isspace(*line)) line++;
+          pi->title = g_strdup(line);
+        }
+        if (!strncmp(line, "Notification-Text:", 18)) {
+          line += 19;
+          while(isspace(*line)) line++;
+          pi->text = g_strdup(line);
+        }
+        if (!strncmp(line, "Notification-Icon:", 18)) {
+          line += 19;
+          while(isspace(*line)) line++;
+          pi->icon = g_strdup(line);
+        }
+        if (!strncmp(line, "Notification-Callback-Target:", 29)) {
+          line += 30;
+          while(isspace(*line)) line++;
+          pi->url = g_strdup(line);
+        }
+      }
+      popup_show(pi);
+      free(data);
+    }
+    ptr = "GNTP/1.0 OK\r\n\r\n";
+    send(pi->sock, ptr, strlen(ptr), 0);
+  } else {
+    ptr = "GNTP/1.0 -ERROR Invalid command\r\n\r\n";
+    send(pi->sock, ptr, strlen(ptr), 0);
+  }
+  free(top);
+  closesocket(pi->sock);
+  return NULL;
 }
 
 int
@@ -449,11 +621,8 @@ main(int argc, char* argv[]) {
   GtkStatusIcon* status_icon;
   GtkWidget* menu;
   GtkWidget* menu_exit;
-#ifdef _WIN32
-  char on;
-#else
-  int on;
-#endif
+  GError* error = NULL;
+  sockopt_t sockopt;
 
   if (argc != 2) {
     fprintf(stderr, "usage: gol [password]\n");
@@ -476,13 +645,13 @@ main(int argc, char* argv[]) {
     exit(1);
   }
 
-  on = 1;
-  if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on)) == -1) {
+  sockopt = 1;
+  if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &sockopt, sizeof(sockopt)) == -1) {
     perror("setsockopt");
     exit(1);
   }
-  on = 1;
-  if (setsockopt(fd, IPPROTO_TCP, TCP_NODELAY, &on, sizeof(on)) == -1) {
+  sockopt = 1;
+  if (setsockopt(fd, IPPROTO_TCP, TCP_NODELAY, &sockopt, sizeof(sockopt)) == -1) {
     perror("setsockopt");
     exit(1);
   }
@@ -502,6 +671,10 @@ main(int argc, char* argv[]) {
     closesocket(fd);
     exit(1);
   }
+
+
+  g_thread_init(NULL);
+  gdk_threads_init();
 
   gtk_init(&argc, &argv);
   status_icon = gtk_status_icon_new_from_file("./data/icon.png");
@@ -529,165 +702,9 @@ main(int argc, char* argv[]) {
       perror("accept");
       continue;
     }
-    on = 1;
-    setsockopt(sock, IPPROTO_TCP, TCP_NODELAY, &on, sizeof(on));
-    char* ptr;
-    int r = readall(sock, &ptr);
-    char* top = ptr;
-    if (!strncmp(ptr, "GNTP/1.0 ", 9)) {
-      ptr += 9;
-      if (!strncmp(ptr, "REGISTER ", 9)) {
-        ptr += 9;
-      } else
-      if (!strncmp(ptr, "NOTIFY ", 7)) {
-        ptr += 7;
-        char* data = NULL;
-        if (!strncmp(ptr, "NONE ", 5)) {
-          ptr = strchr(ptr, '\r');
-          *ptr++ = 0;
-          *ptr++ = 0;
-          data = (char*) calloc(r-(ptr-top)-4+1, 1);
-          memcpy(data, ptr, r-(ptr-top)-4);
-        } else {
-          if (strncmp(ptr, "AES:", 4) &&
-              strncmp(ptr, "DES:", 4) &&
-              strncmp(ptr, "3DES:", 5)) continue;
-
-          char* crypt_algorythm = ptr;
-          ptr = strchr(ptr, ':');
-          *ptr++ = 0;
-          char* iv;
-          iv = ptr;
-          ptr = strchr(ptr, ' ');
-          *ptr++ = 0;
-
-          if (strncmp(ptr, "MD5:", 4) &&
-              strncmp(ptr, "SHA1:", 5) &&
-              strncmp(ptr, "SHA256:", 7)) continue;
-
-          char* hash_algorythm = ptr;
-          ptr = strchr(ptr, ':');
-          *ptr++ = 0;
-          char* key = ptr;
-          ptr = strchr(ptr, '.');
-          *ptr++ = 0;
-          char* salt = ptr;
-
-          ptr = strchr(ptr, '\r');
-          *ptr++ = 0;
-          *ptr++ = 0;
-
-          int n, keylen, saltlen, ivlen;
-
-          char hex[3];
-          hex[2] = 0;
-          saltlen = strlen(salt) / 2;
-          for (n = 0; n < saltlen; n++)
-            salt[n] = unhex(salt[n * 2]) * 16 + unhex(salt[n * 2 + 1]);
-          keylen = strlen(key) / 2;
-          for (n = 0; n < keylen; n++)
-            key[n] = unhex(key[n * 2]) * 16 + unhex(key[n * 2 + 1]);
-          ivlen = strlen(iv) / 2;
-          for (n = 0; n < ivlen; n++)
-            iv[n] = unhex(iv[n * 2]) * 16 + unhex(iv[n * 2 + 1]);
-
-          char digest[32] = {0};
-          memset(digest, 0, sizeof(digest));
-
-          if (!strcmp(hash_algorythm, "MD5")) {
-            MD5_CTX ctx;
-            MD5_Init(&ctx);
-            MD5_Update(&ctx, password, strlen(password));
-            MD5_Update(&ctx, salt, saltlen);
-            MD5_Final((unsigned char*) digest, &ctx);
-          }
-          if (!strcmp(hash_algorythm, "SHA1")) {
-            SHA_CTX ctx;
-            SHA1_Init(&ctx);
-            SHA1_Update(&ctx, password, strlen(password));
-            SHA1_Update(&ctx, salt, saltlen);
-            SHA1_Final((unsigned char*) digest, &ctx);
-          }
-          if (!strcmp(hash_algorythm, "SHA256")) {
-            SHA256_CTX ctx;
-            SHA256_Init(&ctx);
-            SHA256_Update(&ctx, password, strlen(password));
-            SHA256_Update(&ctx, salt, saltlen);
-            SHA256_Final((unsigned char*) digest, &ctx);
-          }
-
-          data = (char*) calloc(r, 1);
-          if (!strcmp(crypt_algorythm, "AES")) {
-            AES_KEY aeskey;
-            AES_set_decrypt_key((unsigned char*) digest, 24 * 8, &aeskey);
-            AES_cbc_encrypt((unsigned char*) ptr, (unsigned char*) data,
-                r-(ptr-top)-6, &aeskey, (unsigned char*) iv, AES_DECRYPT);
-          }
-          if (!strcmp(crypt_algorythm, "DES")) {
-            des_key_schedule schedule;
-            DES_set_key_unchecked((const_DES_cblock*) &digest, &schedule);
-            DES_ncbc_encrypt((unsigned char*) ptr, (unsigned char*) data,
-                r-(ptr-top)-6, &schedule, (const_DES_cblock*) &iv, DES_DECRYPT);
-          }
-          if (!strcmp(crypt_algorythm, "3DES")) {
-            char key1[8], key2[8], key3[8];
-            memcpy(key1, digest+ 0, 8);
-            memcpy(key2, digest+ 8, 8);
-            memcpy(key3, digest+16, 8);
-            des_key_schedule schedule1, schedule2, schedule3;
-            DES_set_key_unchecked((const_DES_cblock*) &key1, &schedule1);
-            DES_set_key_unchecked((const_DES_cblock*) &key2, &schedule2);
-            DES_set_key_unchecked((const_DES_cblock*) &key3, &schedule3);
-            des_ede3_cbc_encrypt((unsigned char*) ptr, (unsigned char*) data,
-                r-(ptr-top)-6, schedule1, schedule2, schedule3,
-                (const_DES_cblock*) &iv, DES_DECRYPT);
-          }
-        }
-
-        ptr = data;
-        char* title = "";
-        char* text = "";
-        char* icon = "";
-        char* url = "";
-        while (*ptr) {
-          char* line = ptr;
-          ptr = strchr(ptr, '\r');
-          if (!ptr) break;
-          *ptr++ = 0;
-          *ptr++ = 0;
-          if (!strncmp(line, "Notification-Title:", 19)) {
-            line += 20;
-            while(isspace(*line)) line++;
-            title = line;
-          }
-          if (!strncmp(line, "Notification-Text:", 18)) {
-            line += 19;
-            while(isspace(*line)) line++;
-            text = line;
-          }
-          if (!strncmp(line, "Notification-Icon:", 18)) {
-            line += 19;
-            while(isspace(*line)) line++;
-            icon = line;
-          }
-          if (!strncmp(line, "Notification-Callback-Target:", 29)) {
-            line += 30;
-            while(isspace(*line)) line++;
-            url = line;
-          }
-        }
-        if (*title && *text)
-          popup_show(title, text, icon, url);
-        free(data);
-      }
-      ptr = "GNTP/1.0 OK\r\n\r\n";
-      send(sock, ptr, strlen(ptr), 0);
-    } else {
-      ptr = "GNTP/1.0 -ERROR Invalid command\r\n\r\n";
-      send(sock, ptr, strlen(ptr), 0);
-    }
-    free(top);
-    closesocket(sock);
+    POPUP_INFO* pi = g_new0(POPUP_INFO, 1);
+    pi->sock = sock;
+    g_thread_create(recv_thread, pi, TRUE, &error);
   }
 
   return 0;
