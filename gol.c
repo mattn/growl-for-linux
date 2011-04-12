4fdf1de33caeb533fa32e9f0566cb7ad9494eab3
mattn <mattn.jp@gmail.com>
2011-04-12 14:41:42 +0900

    split code of display to external module.

--- gol.c
+++ gol.c
@@ -34,7 +34,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <memory.h>
-#include <curl/curl.h>
 #include <sqlite3.h>
 #ifdef _WIN32
 # include <io.h>
@@ -43,6 +42,7 @@
 #include <openssl/sha.h>
 #include <openssl/aes.h>
 #include <openssl/des.h>
+#include "gol.h"
 
 #ifdef _WIN32
 typedef char sockopt_t;
@@ -64,367 +64,20 @@ typedef int socklen_t;
 typedef int sockopt_t;
 #endif
 
-#define REQUEST_TIMEOUT            (5)
-
-static GList* notifications = NULL;
 static gchar* password = NULL;
 static gboolean main_loop = TRUE;
-
-typedef struct {
-  int sock;
-
-  gint pos;
-  gint x, y;
-  gchar* title;
-  gchar* text;
-  gchar* icon;
-  gchar* url;
-  gint timeout;
-
-  GtkWidget* popup;
-  gint offset;
-
-} NOTIFICATION_INFO;
-
-typedef struct {
-  char* data;     // response data from server
-  size_t size;    // response size of data
-} MEMFILE;
-
-static MEMFILE*
-memfopen() {
-  MEMFILE* mf = (MEMFILE*) malloc(sizeof(MEMFILE));
-  if (mf) {
-    mf->data = NULL;
-    mf->size = 0;
-  }
-  return mf;
-}
-
-static void
-memfclose(MEMFILE* mf) {
-  if (mf->data) free(mf->data);
-  free(mf);
-}
-
-static size_t
-memfwrite(char* ptr, size_t size, size_t nmemb, void* stream) {
-  MEMFILE* mf = (MEMFILE*) stream;
-  int block = size * nmemb;
-  if (!mf) return block; // through
-  if (!mf->data)
-    mf->data = (char*) malloc(block);
-  else
-    mf->data = (char*) realloc(mf->data, mf->size + block);
-  if (mf->data) {
-    memcpy(mf->data + mf->size, ptr, block);
-    mf->size += block;
-  }
-  return block;
-}
-
-static char*
-memfstrdup(MEMFILE* mf) {
-  char* buf;
-  if (mf->size == 0) return NULL;
-  buf = (char*) malloc(mf->size + 1);
-  memcpy(buf, mf->data, mf->size);
-  buf[mf->size] = 0;
-  return buf;
-}
-
-static char*
-get_http_header_alloc(const char* ptr, const char* key) {
-  const char* tmp = ptr;
-
-  while (*ptr) {
-    tmp = strpbrk(ptr, "\r\n");
-    if (!tmp) break;
-    if (!strncasecmp(ptr, key, strlen(key)) && *(ptr + strlen(key)) == ':') {
-      size_t len;
-      char* val;
-      const char* top = ptr + strlen(key) + 1;
-      while (*top && isspace(*top)) top++;
-      if (!*top) return NULL;
-      len = tmp - top + 1;
-      val = malloc(len);
-      memset(val, 0, len);
-      strncpy(val, top, len-1);
-      return val;
-    }
-    ptr = tmp + 1;
-  }
-  return NULL;
-}
-
-static GdkPixbuf*
-url2pixbuf(const char* url, GError** error) {
-  GdkPixbuf* pixbuf = NULL;
-  GdkPixbufLoader* loader = NULL;
-  GError* _error = NULL;
-
-  if (!strncmp(url, "file:///", 8) || g_file_test(url, G_FILE_TEST_EXISTS)) {
-    gchar* newurl = g_filename_from_uri(url, NULL, NULL);
-    pixbuf = gdk_pixbuf_new_from_file(newurl ? newurl : url, &_error);
-  } else {
-    CURL* curl = NULL;
-    MEMFILE* mbody;
-    MEMFILE* mhead;
-    char* head;
-    char* body;
-    unsigned long size;
-    CURLcode res = CURLE_FAILED_INIT;
-
-    curl = curl_easy_init();
-    if (!curl) return NULL;
-
-    mbody = memfopen();
-    mhead = memfopen();
-
-    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
-    curl_easy_setopt(curl, CURLOPT_URL, url);
-    curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT, REQUEST_TIMEOUT);
-    curl_easy_setopt(curl, CURLOPT_TIMEOUT, REQUEST_TIMEOUT);
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, memfwrite);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, mbody);
-    curl_easy_setopt(curl, CURLOPT_HEADERFUNCTION, memfwrite);
-    curl_easy_setopt(curl, CURLOPT_HEADERDATA, mhead);
-    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1);
-    res = curl_easy_perform(curl);
-    curl_easy_cleanup(curl);
-
-    head = memfstrdup(mhead);
-    memfclose(mhead);
-    body = memfstrdup(mbody);
-    size = mbody->size;
-    memfclose(mbody);
-
-    if (res == CURLE_OK) {
-      char* ctype;
-      char* csize;
-      ctype = get_http_header_alloc(head, "Content-Type");
-      csize = get_http_header_alloc(head, "Content-Length");
-
-#ifdef _WIN32
-      if (ctype &&
-          (!strcmp(ctype, "image/jpeg") || !strcmp(ctype, "image/gif"))) {
-        char temp_path[MAX_PATH];
-        char temp_filename[MAX_PATH];
-        FILE* fp;
-        GetTempPath(sizeof(temp_path), temp_path);
-        GetTempFileName(temp_path, "growl-for-linux-", 0, temp_filename);
-        fp = fopen(temp_filename, "wb");
-        if (fp) {
-          fwrite(body, size, 1, fp);
-          fclose(fp);
-        }
-        pixbuf = gdk_pixbuf_new_from_file(temp_filename, NULL);
-        DeleteFile(temp_filename);
-      } else
-#endif
-      {
-        if (ctype)
-          loader =
-            (GdkPixbufLoader*) gdk_pixbuf_loader_new_with_mime_type(ctype,
-                error);
-        if (csize)
-          size = atol(csize);
-        if (!loader) loader = gdk_pixbuf_loader_new();
-        if (body && gdk_pixbuf_loader_write(loader, (const guchar*) body,
-              size, &_error)) {
-          pixbuf = gdk_pixbuf_loader_get_pixbuf(loader);
-        }
-      }
-      if (ctype) free(ctype);
-      if (csize) free(csize);
-      if (loader) gdk_pixbuf_loader_close(loader, NULL);
-    } else {
-      _error = g_error_new_literal(G_FILE_ERROR, res,
-          curl_easy_strerror(res));
-    }
-
-    free(head);
-    free(body);
-  }
-
-  /* cleanup callback data */
-  if (error && _error) *error = _error;
-  return pixbuf;
-}
-
-static gboolean
-open_url(const gchar* url) {
-#if defined(_WIN32)
-  return (int) ShellExecute(NULL, "open", url, NULL, NULL, SW_SHOW) > 32;
-#elif defined(MACOSX)
-  GError* error = NULL;
-  const gchar *argv[] = {"open", (gchar*) url, NULL};
-  return g_spawn_async(NULL, argv, NULL, G_SPAWN_SEARCH_PATH, NULL, NULL, NULL, &error);
-#else
-  GError* error = NULL;
-  gchar *argv[] = {"xdg-open", (gchar*) url, NULL};
-  return g_spawn_async(NULL, argv, NULL, G_SPAWN_SEARCH_PATH, NULL, NULL, NULL, &error);
-#endif
-}
-
-static void
-notification_clicked(GtkWidget* widget, GdkEvent* event, gpointer user_data) {
-  NOTIFICATION_INFO* ni = (NOTIFICATION_INFO*) user_data;
-  if (ni->timeout >= 30) ni->timeout = 30;
-  if (ni->url && *ni->url) open_url(ni->url);
-}
-
-static gboolean
-notification_animation_func(gpointer data) {
-  NOTIFICATION_INFO* ni = (NOTIFICATION_INFO*) data;
-
-  if (ni->timeout-- < 0) {
-    gtk_widget_destroy(ni->popup);
-    notifications = g_list_remove(notifications, ni);
-    g_free(ni->title);
-    g_free(ni->text);
-    g_free(ni->icon);
-    g_free(ni->url);
-    g_free(ni);
-    return FALSE;
-  }
-
-  if (ni->offset < 160) {
-    ni->offset += 2;
-    gtk_window_resize(GTK_WINDOW(ni->popup), 180, ni->offset);
-    gtk_window_move(GTK_WINDOW(ni->popup), ni->x, ni->y - ni->offset);
-  }
-
-  if (ni->timeout < 30) {
-    gtk_window_set_opacity(GTK_WINDOW(ni->popup), (double) ni->timeout/30.0*0.8);
-  }
-  return TRUE;
-}
-
-static gint
-notifications_compare(gconstpointer a, gconstpointer b) {
-  return ((NOTIFICATION_INFO*)b)->pos < ((NOTIFICATION_INFO*)a)->pos;
-}
-
-static gint
-notification_show(gpointer data) {
-  NOTIFICATION_INFO* ni = (NOTIFICATION_INFO*) data;
-
-  GdkColor color;
-  GtkWidget* fixed;
-  GtkWidget* vbox;
-  GtkWidget* hbox;
-  GtkWidget* label;
-  GtkWidget* image;
-  GdkPixbuf* pixbuf;
-  GdkScreen* screen;
-  gint n, pos, len;
-  gint x, y;
-  gint monitor_num;
-  GdkRectangle rect;
-
-  len = g_list_length(notifications);
-  for (pos = 0; pos < len; pos++) {
-    NOTIFICATION_INFO* p = g_list_nth_data(notifications, pos);
-    if (pos != p->pos) break;
-  }
-
-  screen = gdk_screen_get_default();
-  monitor_num = gdk_screen_get_primary_monitor(screen);
-  gdk_screen_get_monitor_geometry(screen, monitor_num, &rect);
-
-  x = rect.x + rect.width - 180;
-  y = rect.y + rect.height - 180;
-  for (n = 0; n < pos; n++) {
-    y -= 180;
-    if (y < 0) {
-      x -= 200;
-      if (x < 0) {
-        return FALSE;
-      }
-      y = rect.y + rect.height - 180;
-    }
-  }
-
-  ni->pos = pos;
-  notifications = g_list_insert_sorted(notifications, ni, notifications_compare);
-  ni->x = x;
-  ni->y = y + 200;
-
-  ni->popup = gtk_window_new(GTK_WINDOW_POPUP);
-  gtk_window_set_title(GTK_WINDOW(ni->popup), "growl-for-linux");
-  gtk_window_set_resizable(GTK_WINDOW(ni->popup), TRUE);
-  gtk_window_set_decorated(GTK_WINDOW(ni->popup), FALSE);
-  gtk_window_set_keep_above(GTK_WINDOW(ni->popup), TRUE);
-
-  gtk_window_stick(GTK_WINDOW(ni->popup));
-  gtk_window_set_opacity(GTK_WINDOW(ni->popup), 0.8);
-  gdk_color_parse ("white", &color);
-  gtk_widget_modify_bg(ni->popup, GTK_STATE_NORMAL, &color);
-  gdk_color_parse ("black", &color);
-  gtk_widget_modify_fg(ni->popup, GTK_STATE_NORMAL, &color);
-
-  fixed = gtk_fixed_new();
-  gtk_container_set_border_width(GTK_CONTAINER(fixed), 10);
-  gtk_container_add(GTK_CONTAINER(ni->popup), fixed);
-
-  vbox = gtk_vbox_new(FALSE, 5);
-  gtk_container_add(GTK_CONTAINER(fixed), vbox);
-
-  hbox = gtk_hbox_new(FALSE, 5);
-
-  if (ni->icon && *ni->icon) {
-    pixbuf = url2pixbuf(ni->icon, NULL);
-    if (pixbuf) {
-      GdkPixbuf* tmp = gdk_pixbuf_scale_simple(pixbuf, 32, 32, GDK_INTERP_TILES);
-      if (tmp) pixbuf = tmp;
-      image = gtk_image_new_from_pixbuf(pixbuf);
-      gtk_box_pack_start(GTK_BOX(hbox), image, FALSE, FALSE, 0);
-    }
-  }
-
-//  PangoFontDescription* font_desc = pango_font_description_new();
-//  pango_font_description_set_family(font_desc, "cinecaption 20");
-
-  label = gtk_label_new(ni->title);
-//  gtk_widget_modify_font(label, font_desc);
-  gtk_box_pack_start(GTK_BOX(hbox), label, FALSE, FALSE, 0);
-
-  gtk_box_pack_start(GTK_BOX(vbox), hbox, FALSE, FALSE, 0);
-
-  label = gtk_label_new(ni->text);
-//  gtk_widget_modify_font(label, font_desc);
-  gtk_label_set_line_wrap(GTK_LABEL(label), TRUE);
-  gtk_label_set_line_wrap_mode(GTK_LABEL(label), PANGO_WRAP_CHAR);
-  gtk_box_pack_start(GTK_BOX(vbox), label, TRUE, FALSE, 0);
-
-//  pango_font_description_free(font_desc);
-
-  gtk_widget_set_size_request(ni->popup, 180, 1);
-
-  gtk_widget_set_events(ni->popup, GDK_BUTTON_PRESS_MASK);
-  g_signal_connect(G_OBJECT(ni->popup), "button-press-event", G_CALLBACK(notification_clicked), ni);
-
-  ni->offset = 0;
-  ni->timeout = 500;
-
-  gtk_window_move(GTK_WINDOW(ni->popup), ni->x, ni->y);
-  gtk_widget_show_all(ni->popup);
-
-#ifdef _WIN32
-  SetWindowPos(GDK_WINDOW_HWND(ni->popup->window), HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
-#endif
-  
-  g_timeout_add(10, notification_animation_func, ni);
-
-  return FALSE;
-}
+typedef gboolean (*notification_init_fn)(gchar* datadir);
+static notification_init_fn notification_init = NULL;
+typedef gboolean (*notification_show_fn)(NOTIFICATION_INFO* ni);
+static notification_show_fn notification_show = NULL;
+typedef gboolean (*notification_term_fn)();
+static notification_term_fn notification_term = NULL;
 
 static long
 readall(int fd, char** ptr) {
   int i = 0, r;
   *ptr = (char*) calloc(BUFSIZ, 1);
-  while ((r = recv(fd, *ptr + i, BUFSIZ, 0)) > 0) {
+  while (*ptr && (r = recv(fd, *ptr + i, BUFSIZ, 0)) > 0) {
     i += r;
     if (r > 2 && !strncmp(*ptr + i - 4, "\r\n\r\n", 4)) break;
     *ptr = realloc(*ptr, BUFSIZ + i);
@@ -530,11 +183,11 @@ recv_thread(gpointer data) {
   if (!ni) {
     perror("g_new0");
   }
-  ni->sock = sock;
 
   char* ptr;
-  int r = readall(ni->sock, &ptr);
+  int r = readall(sock, &ptr);
   char* top = ptr;
+  char* end = ptr + r;
   if (!strncmp(ptr, "GNTP/1.0 ", 9)) {
     ptr += 9;
     if (!strncmp(ptr, "REGISTER ", 9)) {
@@ -546,8 +199,9 @@ recv_thread(gpointer data) {
       char* data = NULL;
       if (!strncmp(ptr, "NONE", 4) && strchr("\n ", *(ptr+5))) {
         ptr = strchr(ptr, '\r');
-        if (!ptr) goto leave;
+        if (!ptr || ptr > end) goto leave;
         *ptr++ = 0;
+        if (*ptr != '\n') goto leave;
         *ptr++ = 0;
         data = (char*) calloc(r-(ptr-top)-4+1, 1);
         if (!data) goto leave;
@@ -559,12 +213,12 @@ recv_thread(gpointer data) {
 
         char* crypt_algorythm = ptr;
         ptr = strchr(ptr, ':');
-        if (!ptr) goto leave;
+        if (!ptr || ptr > end) goto leave;
         *ptr++ = 0;
         char* iv;
         iv = ptr;
         ptr = strchr(ptr, ' ');
-        if (!ptr) goto leave;
+        if (!ptr || ptr > end) goto leave;
         *ptr++ = 0;
 
         if (strncmp(ptr, "MD5:", 4) &&
@@ -573,17 +227,18 @@ recv_thread(gpointer data) {
 
         char* hash_algorythm = ptr;
         ptr = strchr(ptr, ':');
-        if (!ptr) goto leave;
+        if (!ptr || ptr > end) goto leave;
         *ptr++ = 0;
         char* key = ptr;
         ptr = strchr(ptr, '.');
-        if (!ptr) goto leave;
+        if (!ptr || ptr > end) goto leave;
         *ptr++ = 0;
         char* salt = ptr;
 
         ptr = strchr(ptr, '\r');
-        if (!ptr) goto leave;
+        if (!ptr || ptr > end) goto leave;
         *ptr++ = 0;
+        if (*ptr != '\n') goto leave;
         *ptr++ = 0;
 
         int n, keylen, saltlen, ivlen;
@@ -658,8 +313,9 @@ recv_thread(gpointer data) {
       while (*ptr) {
         char* line = ptr;
         ptr = strchr(ptr, '\r');
-        if (!ptr) break;
+        if (!ptr) goto leave;
         *ptr++ = 0;
+        if (*ptr != '\n') goto leave;
         *ptr++ = 0;
         if (!strncmp(line, "Notification-Title:", 19)) {
           line += 20;
@@ -688,20 +344,20 @@ recv_thread(gpointer data) {
       else {
         ptr = "GNTP/1.0 -ERROR Invalid data\r\n"
             "Error-Description: Invalid data\r\n\r\n";
-        send(ni->sock, ptr, strlen(ptr), 0);
+        send(sock, ptr, strlen(ptr), 0);
       }
       free(data);
     }
     ptr = "GNTP/1.0 OK\r\n\r\n";
-    send(ni->sock, ptr, strlen(ptr), 0);
+    send(sock, ptr, strlen(ptr), 0);
   } else {
     ptr = "GNTP/1.0 -ERROR Invalid command\r\n\r\n";
-    send(ni->sock, ptr, strlen(ptr), 0);
+    send(sock, ptr, strlen(ptr), 0);
   }
   free(top);
-  closesocket(ni->sock);
+  closesocket(sock);
   if (need_to_show)
-    g_idle_add(notification_show, ni); // call once
+    g_idle_add((GSourceFunc) notification_show, ni); // call once
   else {
     g_free(ni->title);
     g_free(ni->text);
@@ -713,7 +369,7 @@ recv_thread(gpointer data) {
 
 leave:
   free(top);
-  closesocket(ni->sock);
+  closesocket(sock);
   free(ni);
   return NULL;
 }
@@ -723,6 +379,7 @@ signal_handler(int num) {
   main_loop = FALSE;
 }
 
+/*
 static GdkPixbuf*
 disabled_pixbuf(GdkPixbuf *pixbuf) {
   GdkPixbuf* gray = gdk_pixbuf_copy(pixbuf);
@@ -749,6 +406,7 @@ disabled_pixbuf(GdkPixbuf *pixbuf) {
   }
   return gray;
 }
+*/
 
 int
 main(int argc, char* argv[]) {
@@ -769,39 +427,9 @@ main(int argc, char* argv[]) {
   WSAStartup(MAKEWORD(2, 2), &wsaData);
 #endif
 
-  if ((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-    perror("socket");
-    exit(1);
-  }
-
-  sockopt = 1;
-  if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &sockopt, sizeof(sockopt)) == -1) {
-    perror("setsockopt");
-    exit(1);
-  }
-  sockopt = 1;
-  if (setsockopt(fd, IPPROTO_TCP, TCP_NODELAY, &sockopt, sizeof(sockopt)) == -1) {
-    perror("setsockopt");
-    exit(1);
-  }
-
-  memset((char *) &server_addr, 0, sizeof(server_addr));
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-  server_addr.sin_port = htons(23053);
-
-  if (bind(fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-    perror("bind");
-    exit(1);
-  }
-
-  if (listen(fd, 5) < 0) {
-    perror("listen");
-    closesocket(fd);
-    exit(1);
-  }
-
+#ifdef G_THREADS_ENABLED
   g_thread_init(NULL);
+#endif
 
   gtk_init(&argc, &argv);
   // TODO: absolute path
@@ -857,8 +485,57 @@ main(int argc, char* argv[]) {
     password = g_strdup("");
   }
 
+  //gchar* path = g_module_build_path("./display/default", "default");
+  gchar* path = g_module_build_path("./display/balloon", "balloon");
+  GModule* handle = g_module_open(path, G_MODULE_BIND_LAZY);
+  g_free(path);
+  if (!g_module_symbol(handle, "notification_show", (void**) &notification_show)) {
+    g_module_close(handle);
+    perror("g_module_open");
+    exit(1);
+  }
+  g_module_symbol(handle, "notification_init", (void**) &notification_init);
+  g_module_symbol(handle, "notification_term", (void**) &notification_term);
+  g_module_make_resident(handle);
+
+  if (notification_init) {
+    notification_init("./display/balloon");
+  }
+
   signal(SIGTERM, signal_handler);
   signal(SIGINT, signal_handler);
+
+  if ((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    perror("socket");
+    exit(1);
+  }
+
+  sockopt = 1;
+  if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &sockopt, sizeof(sockopt)) == -1) {
+    perror("setsockopt");
+    exit(1);
+  }
+  sockopt = 1;
+  if (setsockopt(fd, IPPROTO_TCP, TCP_NODELAY, &sockopt, sizeof(sockopt)) == -1) {
+    perror("setsockopt");
+    exit(1);
+  }
+
+  memset((char *) &server_addr, 0, sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  server_addr.sin_port = htons(23053);
+
+  if (bind(fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+    perror("bind");
+    exit(1);
+  }
+
+  if (listen(fd, 5) < 0) {
+    perror("listen");
+    closesocket(fd);
+    exit(1);
+  }
 
   while (main_loop) {
     gtk_main_iteration_do(FALSE);
@@ -881,14 +558,16 @@ main(int argc, char* argv[]) {
     sockopt = 1;
     setsockopt(sock, IPPROTO_TCP, TCP_NODELAY, &sockopt, sizeof(sockopt));
 
+#ifdef G_THREADS_ENABLED
     g_thread_create(recv_thread, (gpointer) sock, FALSE, NULL);
+#else
+    recv_thread((gpointer) sock);
+#endif
   }
 
   sqlite3_close(db);
 
   gtk_status_icon_set_visible(GTK_STATUS_ICON(status_icon), FALSE);
-
-  gdk_threads_leave();
 
 #ifdef _WIN32
   WSACleanup();
