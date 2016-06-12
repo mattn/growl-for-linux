0bd6e53b92c202d68215cc13b262bbf1ccc06b23
Kentaro Hayashi <kenhys@gmail.com>
2016-06-12 12:40:03 +0900

    Fix coding style

--- gol.c
+++ gol.c
@@ -1152,7 +1152,7 @@ about_click(GtkWidget* GOL_UNUSED_ARG(widget), gpointer GOL_UNUSED_ARG(user_data
       "http://mattn.kaoriya.net/");
   {
     GdkPixbuf* logo = pixbuf_from_datadir("icon.png", NULL);
-    gtk_about_dialog_set_logo (GTK_ABOUT_DIALOG(about_dialog), logo);
+    gtk_about_dialog_set_logo(GTK_ABOUT_DIALOG(about_dialog), logo);
     g_object_unref(G_OBJECT(logo));
   }
   gtk_window_set_position(GTK_WINDOW(about_dialog), GTK_WIN_POS_CENTER);
